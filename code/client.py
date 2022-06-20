5622abf00d1f2e052002bae11f315cfcfb2402ca
Daniil-Solo <daniil.solo1723@gmail.com>
2022-06-20 08:12:13 +0500

    add tcp-sockets, this works!

--- code/client.py
+++ code/client.py
@@ -4,7 +4,7 @@ import time
 
 from utils import calculate_limit
 from storage import Storage
-from configs import BUF_SIZE, KEY_PHRASE, SERVER_PORT, TIMEOUT
+from configs import BUF_SIZE, KEY_PHRASE, SERVER_PORT, TIMEOUT, SERVER_TCP_PORT
 
 
 SAVING_DIR = os.path.join(os.getcwd(), './my_saves')
@@ -16,6 +16,9 @@ class Client:
         self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.__socket.settimeout(TIMEOUT)
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         self.server_address = None
         self.__msgs_of_client = Storage(SAVING_DIR)
         self.__username = username
@@ -23,18 +26,21 @@ class Client:
         self.connected = True
         self.data = None
         self.lock = lock
-        self.file_waiting_from_username = ''
 
     def get_server_address(self):
         """
         Получение адреса сервера
         """
-        self.__socket.sendto((KEY_PHRASE + self.__username).encode(), ('<broadcast>', SERVER_PORT))
+        self.__socket.sendto(KEY_PHRASE.encode(), ('<broadcast>', SERVER_PORT))
         while True:
             data, server = self.__socket.recvfrom(BUF_SIZE)
             if data:
                 self.server_address = server
                 break
+        self.__socket.close()
+        print((self.server_address[0], SERVER_TCP_PORT))
+        self.server_socket.connect((self.server_address[0], SERVER_TCP_PORT))
+        self.server_socket.send(self.__username.encode())
 
     def set_data(self, data):
         """
@@ -47,72 +53,37 @@ class Client:
         Цикл обработки сообщений
         """
         while self.connected:
-            try:
-                data, server = self.__socket.recvfrom(BUF_SIZE)
-            except socket.timeout as ex:
-                pass
-            except ConnectionResetError as ex:
-                print(ex)
-                pass
-            else:
-                print('клиент получил новое сообщение', data[:15])
-                self.__handle_new_data(data)
-        self.__socket.close()
+            data = self.server_socket.recv(BUF_SIZE)
+            self.__handle_new_data(data)
+        self.server_socket.close()
 
     def __handle_new_data(self, data: bytes):
         """
         Обработка нового сообщения
         """
-        if self.file_waiting_from_username != '':
-            print('клиент ожидает файлов')
+        message = data.decode()
+        if message.startswith('FILE'):
+            _, file_size, username = message.split('#')
+            file_size = int(file_size)
+            total = 0
+            data = self.server_socket.recv(BUF_SIZE)
+            total = total + len(data)
+            self.__msgs_of_client[username] = data
+            while total != file_size:
+                data = self.server_socket.recv(BUF_SIZE)
+                total = total + len(data)
+                self.__msgs_of_client.add(username, data)
+
+            filename = self.server_socket.recv(BUF_SIZE).decode()
+            filepath = self.__msgs_of_client[username]
             try:
-                part_msg = data[:4].decode()
-                if part_msg == 'FEND':
-                    print('клиент получил конец файла')
-                    self.wait_file = ''
-                    filename = data.decode().split('FEND')[1]
-                    filepath = self.__msgs_of_client[self.file_waiting_from_username]
-                    try:
-                        os.rename(filepath, os.path.join(SAVING_DIR, filename))
-                        del self.__msgs_of_client[self.file_waiting_from_username]
-                    except FileExistsError as ex:
-                        print(ex)
-                    print('клиент вывел сообщение о полученном файле', filename)
-                    self.print_message(self.file_waiting_from_username + ' отправил файл ' + filename)
-                else:
-                    print('клиент получил часть файла')
-                    if self.file_waiting_from_username not in self.__msgs_of_client:
-                        print('Это часть была первой')
-                        self.__msgs_of_client[self.file_waiting_from_username] = data
-                    else:
-                        print('Это часть была в середине')
-                        self.__msgs_of_client.add(self.file_waiting_from_username, data)
-            except UnicodeDecodeError:
-                print('клиент получил часть файла')
-                if self.file_waiting_from_username not in self.__msgs_of_client:
-                    print('Это часть была первой')
-                    self.__msgs_of_client[self.file_waiting_from_username] = data
-                else:
-                    print('Это часть была в середине')
-                    self.__msgs_of_client.add(self.file_waiting_from_username, data)
+                os.rename(filepath, os.path.join(SAVING_DIR, filename))
+                del self.__msgs_of_client[username]
+            except FileExistsError as ex:
+                print(ex)
+            self.print_message(username + ' отправил файл ' + filename)
         else:
-            print('клиент не ждет файлов')
-            try:
-                part_msg = data[:100].decode()
-                if part_msg.find('FILE') != -1:
-                    print('клиент начинает ждать файл от', part_msg.split('FILE')[0])
-                    self.file_waiting_from_username = part_msg.split('FILE')[0]
-                else:
-                    print('клиент вывел текстовое сообщение')
-                    self.print_message(data.decode())
-            except UnicodeDecodeError:
-                print('клиент получил часть файла')
-                if self.file_waiting_from_username not in self.__msgs_of_client:
-                    print('Это часть была первой')
-                    self.__msgs_of_client[self.file_waiting_from_username] = data
-                else:
-                    print('Это часть была в середине')
-                    self.__msgs_of_client.add(self.file_waiting_from_username, data)
+            self.print_message(data.decode())
 
     def print_message(self, text: str):
         """
@@ -127,7 +98,7 @@ class Client:
         Отправка текстового сообщения
         """
         data = ('t' + text).encode()
-        self.__socket.sendto(data, self.server_address)
+        self.server_socket.send(data)
 
     def send_file(self, filepath: str, progressbar):
         """
@@ -136,13 +107,15 @@ class Client:
         file_size = os.path.getsize(filepath)
         add_part_size = self.__limit / file_size * 100
         sent_size = 0
+        data = ('FILE' + str(file_size)).encode()
+        self.server_socket.send(data)
         with open(filepath, 'rb') as f:
             file_part = f.read(self.__limit)
             while file_part:
-                self.__socket.sendto(file_part, self.server_address)
+                self.server_socket.send(file_part)
                 time.sleep(TIMEOUT)
                 file_part = f.read(self.__limit)
                 sent_size += add_part_size
                 progressbar[0] = int(sent_size)
-        data = ('FEND' + os.path.basename(filepath)).encode()
-        self.__socket.sendto(data, self.server_address)
+        data = (os.path.basename(filepath)).encode()
+        self.server_socket.send(data)
--- code/server.py
+++ code/server.py
@@ -4,20 +4,22 @@ import threading
 import time
 
 from utils import calculate_limit
-from storage import Storage
-from configs import BUF_SIZE, KEY_PHRASE, SERVER_PORT as PORT, TIMEOUT
+from configs import BUF_SIZE, KEY_PHRASE, SERVER_PORT as PORT, TIMEOUT, SERVER_TCP_PORT
 
 IP = ""
-SAVING_DIR = os.path.join(os.getcwd(), './saves')
 
 
 class Server:
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, port_tcp):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.socket.bind((ip, port))
-        self.msgs_of_client = Storage(SAVING_DIR)
+        self.socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket_tcp.bind((ip, port_tcp))
+        self.socket_tcp.listen(10)
+
         self.clients = dict()
         print("Сервер настроился!")
 
@@ -25,76 +27,77 @@ class Server:
         """
         Основной цикл обработки входящих сообщений
         """
-        data, client = None, None
         print("Сервер запущен!")
+        threading.Thread(target=self.start_tcp).start()
+        threading.Thread(target=self.start_udp).start()
+
+    def start_tcp(self):
+        while True:
+            connection, address = self.socket_tcp.accept()
+            print(connection, address)
+            threading.Thread(target=self.clientThread, args=(connection, address)).start()
+
+    def start_udp(self):
         while True:
             try:
                 data, client = self.socket.recvfrom(BUF_SIZE)
             except socket.error:
                 pass
             else:
-                try:
-                    if data[: len(KEY_PHRASE)].decode() == KEY_PHRASE:
-                        username = data[len(KEY_PHRASE):].decode()
-                        self.clients[client] = username
-                        print('Сервер отправил свой адрес клиенту: ', client)
-                        self.socket.sendto(KEY_PHRASE.encode(), client)
-                        continue
-                    else:
-                        self.handle_new_data(data, client)
-                except UnicodeDecodeError:
-                    self.handle_new_data(data, client)
+                if data[: len(KEY_PHRASE)].decode() == KEY_PHRASE:
+                    print('Сервер отправил свой адрес клиенту: ')
+                    self.socket.sendto(KEY_PHRASE.encode(), client)
+                    continue
 
-    def handle_new_data(self, data: bytes, client: tuple):
+    def clientThread(self, connection, client):
+        data = connection.recv(BUF_SIZE)
+        self.clients[client] = (data.decode(), connection)
+
+        while True:
+            try:
+                data = connection.recv(BUF_SIZE)
+                self.handle_new_data(data, client, connection)
+            except Exception as e:
+                print(e)
+                break
+
+    def handle_new_data(self, data: bytes, client: tuple, connection):
         """
         Обработка нового сообщения
         """
-        try:
-            if data[:1].decode() == 't':
-                self.send_message(data, client,)
-            elif data[:4].decode() == 'FEND':
-                self.send_message(data, client, filename=data[4:].decode())
-            else:
-                username = self.clients[client]
-                if username not in self.msgs_of_client:
-                    self.msgs_of_client[username] = data
-                else:
-                    self.msgs_of_client.add(username, data)
-        except UnicodeDecodeError:
-            username = self.clients[client]
-            if username not in self.msgs_of_client:
-                self.msgs_of_client[username] = data
-            else:
-                self.msgs_of_client.add(username, data)
+        username = self.clients[client][0]
+        other_connections = [(self.clients[c][1], c) for c in self.clients.keys() if c != client]
 
-    def send_message(self, data: bytes, client: tuple, filename=None):
-        """
-        Отправка сообщения всем клиентам чата
-        """
-        username = self.clients[client]
-        clients_for_message = [c for c in self.clients.keys() if c != client]
-        if not filename:
+        if data[:1].decode() == 't':
             new_data = (username + ': ').encode() + data[1:]
-            for some_client in clients_for_message:
-                self.socket.sendto(new_data, some_client)
-        else:
-            new_data = (username + 'FILE').encode()
-            for some_client in clients_for_message:
-                self.socket.sendto(new_data, some_client)
-            with open(self.msgs_of_client[username], 'rb') as f:
-                limit = calculate_limit(username)
-                file_part = f.read(limit)
-                while file_part:
-                    for some_client in clients_for_message:
-                        self.socket.sendto(file_part, some_client)
-                        time.sleep(TIMEOUT)
-                    file_part = f.read(limit)
-            new_data = ('FEND' + filename).encode()
-            for some_client in clients_for_message:
-                self.socket.sendto(new_data, some_client)
-            del self.msgs_of_client[username]
+            self.send_data_to_connections(new_data, other_connections)
+        elif data[:4].decode() == 'FILE':
+            file_size = int(data[4:].decode())
+            new_data = ('FILE#' + str(file_size) + '#' + username).encode()
+            self.send_data_to_connections(new_data, other_connections)
+            total = 0
+            while total != file_size:
+                new_data = connection.recv(BUF_SIZE)
+                total = total + len(new_data)
+                self.send_data_to_connections(new_data, other_connections)
+            file_name = connection.recv(BUF_SIZE)
+            self.send_data_to_connections(file_name, other_connections)
+
+    def send_data_to_connections(self, data, connections):
+        """
+        Отправка сообщения на все подключения
+        """
+        for some_connection, some_client in connections:
+            try:
+                some_connection.send(data)
+            except:
+                some_connection.close()
+                self.remove_connection(some_client)
+
+    def remove_connection(self, client):
+        del self.clients[client]
 
 
 if __name__ == "__main__":
-    server = Server(IP, PORT)
+    server = Server(IP, PORT, SERVER_TCP_PORT)
     server.start()
